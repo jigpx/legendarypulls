46af4b755a9e34ff61f178c9a7c664d3741f318c
jigpx <anditsjig@gmail.com>
2026-02-20 04:20:58 -0500

    Fix deployment configuration and create working Vercel deployment

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  trailingSlash: false,
+  skipMiddlewareUrlNormalize: false,
+  skipTrailingSlashRedirect: false,
+}
+
+module.exports = nextConfig
